b791567d3c1bcb8f9bc40c4afc1ebe8c16ce7e1c
Marek Nedvěd <nedvem@gmail.com>
2024-12-23 00:55:37 +0000

    feat: game basic logic

--- number_guess.sh
+++ number_guess.sh
@@ -29,9 +29,21 @@ NUMBER_GUESS() {
   echo "Guess the secret number between 1 and 1000:"
   read GUESS
 
-  NUMBER=$(( $RANDOM % 1000 ))
-  echo $NUMBER
+  if [[ $GUESS -lt $NUMBER ]]
+  than
+    echo "It's higher than that, guess again:"
+    NUMBER_GUESS
+  elif [[ $GUESS -gt $NUMBER ]]
+  than
+    echo "It's lower than that, guess again:"
+    NUMBER_GUESS
+  else
+    echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"    
+  fi
 }
+
+NUMBER=$(( $RANDOM % 1000 ))
+echo $NUMBER
 
 USER_LOGIN
 NUMBER_GUESS
